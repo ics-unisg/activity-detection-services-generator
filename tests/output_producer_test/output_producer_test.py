2b109a273e214cc8c7fde6d55b402d54e89f18d8
Aaron Kurz <aaronkurz@posteo.de>
2025-05-16 12:12:37 +0200

    Adjust tests

--- tests/output_producer_test/output_producer_test.py
+++ tests/output_producer_test/output_producer_test.py
@@ -60,7 +60,7 @@ select "LowLevel-Pattern-2" as event, "paper act" as activity, e1.timestamp as t
 insert into DetectedLowLevelActivityEvents;
 
 @info(name="Detect-HighLevel-Pattern-2")
-from every e1 = DetectedHighLevelActivityEvents[event == "HighLevel-Pattern-1"] -> not HelperStream[event == "HighToLow"] and e2 = DetectedLowLevelActivityEvents[event == "LowLevel-Pattern-2" and time:timestampInMilliseconds(e1.ts_second, 'yyyy-MM-dd HH:mm:ss.SS') <= time:timestampInMilliseconds(ts_second, 'yyyy-MM-dd HH:mm:ss.SS')]
+from every e1 = DetectedHighLevelActivityEvents[event == "HighLevel-Pattern-1"] -> not DetectedHighLevelActivityEvents[event == "HighLevel-Pattern-1"] and e2 = DetectedLowLevelActivityEvents[event == "LowLevel-Pattern-2" and time:timestampInMilliseconds(e1.ts_second, 'yyyy-MM-dd HH:mm:ss.SS') <= time:timestampInMilliseconds(ts_second, 'yyyy-MM-dd HH:mm:ss.SS')]
 select "HighLevel-Pattern-2" as event,  "LowLevel-Pattern-3" as next_pattern, "paper act" as activity, e2.ts_first as ts_first, e2.ts_second as ts_second
 insert into DetectedHighLevelActivityEvents;
 
@@ -70,12 +70,12 @@ select "LowLevel-Pattern-3" as event, "paper act" as activity, e1.timestamp as t
 insert into DetectedLowLevelActivityEvents;
 
 @info(name="Detect-HighLevel-Pattern-3")
-from every e1 = DetectedHighLevelActivityEvents[event == "HighLevel-Pattern-2"] -> not HelperStream[event == "HighToLow"] and e2 = DetectedLowLevelActivityEvents[event == "LowLevel-Pattern-3" and time:timestampInMilliseconds(e1.ts_second, 'yyyy-MM-dd HH:mm:ss.SS') <= time:timestampInMilliseconds(ts_second, 'yyyy-MM-dd HH:mm:ss.SS')]
+from every e1 = DetectedHighLevelActivityEvents[event == "HighLevel-Pattern-2"] -> not DetectedHighLevelActivityEvents[event == "HighLevel-Pattern-1"] and e2 = DetectedLowLevelActivityEvents[event == "LowLevel-Pattern-3" and time:timestampInMilliseconds(e1.ts_second, 'yyyy-MM-dd HH:mm:ss.SS') <= time:timestampInMilliseconds(ts_second, 'yyyy-MM-dd HH:mm:ss.SS')]
 select "HighLevel-Pattern-3" as event,  "LowLevel-Pattern-4" as next_pattern, "paper act" as activity, e2.ts_first as ts_first, e2.ts_second as ts_second
 insert into DetectedHighLevelActivityEvents;
 
 @info(name="Detect-AllHighLevelPattern-InstanceLevelActivity")
-from every e1 = DetectedHighLevelActivityEvents[event == "HighLevel-Pattern-1"] -> not HelperStream[event == "HighToLow"] and e2 = DetectedHighLevelActivityEvents[event == "HighLevel-Pattern-3"]
+from every e1 = DetectedHighLevelActivityEvents[event == "HighLevel-Pattern-1"] -> not DetectedHighLevelActivityEvents[event == "HighLevel-Pattern-1"] and e2 = DetectedHighLevelActivityEvents[event == "HighLevel-Pattern-3"]
 select "paper act" as activity, "AllHighLevelPattern" as detection_type, e1.ts_second as ts_start, time:timestampInMilliseconds(e1.ts_second, 'yyyy-MM-dd HH:mm:ss.SS') as ts_start_unix, e2.ts_second as ts_end, time:timestampInMilliseconds(e2.ts_second, 'yyyy-MM-dd HH:mm:ss.SS') as ts_end_unix
 insert into DetectedInstanceLevelActivities;
 """
